23be300f4cf806a60e8337683e9ed9e49002339c
Max <50140896+MaxBGitHub@users.noreply.github.com>
2020-10-27 10:01:02 +0100

    Removed unused using directives and references.

--- Petersilie.ManagementTools.NetworkMonitor/NetworkMonitor.cs
+++ Petersilie.ManagementTools.NetworkMonitor/NetworkMonitor.cs
@@ -1,14 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 using System.Net.NetworkInformation;
-using System.Timers;
-using System.Runtime.InteropServices;
-using Petersilie.ManagementTools.NetworkMonitor;
 
 
 namespace Petersilie.ManagementTools.NetworkMonitor
